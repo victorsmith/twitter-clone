6ae70fb5ffb8f3002996d83c5cd9d6adc06e722f
victorsmith <victor7smith@gmail.com>
2022-12-23 01:28:51 -0700

    ugly dashboard works

--- components/tweet.js
+++ components/tweet.js
@@ -7,25 +7,33 @@ export default function Tweet({ tweet }) {
   };
 
   return (
-    <div className="m-1 flex flex-col bg-slate-800 p-1">
-      <div className="flex grow flex-row justify-between">
+    <div className="m-4 flex flex-col rounded-md bg-slate-800 p-4">
+      {/* Top Row */}
+      <div className="flex grow flex-row space-x-3 align-middle">
         <Image
           loader={myLoader}
           src="me.png"
           alt="Picture of the author"
-          width={20}
-          height={20}
+          width={30}
+          height={300}
         />
-        <h6 className="px-2">{tweet.author}</h6>
+        <div className="flex items-center justify-center font-mono text-base font-bold">
+          {tweet.author.username}
+        </div>
       </div>
 
-      <p>{tweet.content}</p>
-      <div className="flex justify-between">
-        <div>
+      {/* Tweet Content */}
+      <div className="my-2 rounded-lg bg-slate-600 p-2">
+        <p className="">{tweet.content}</p>
+      </div>
+
+      {/* Bottom row - Tweet Interactions */}
+      <div className="flex justify-around">
+        <div className="my-2 rounded-lg bg-slate-600 p-2">
           Replies: <span>{tweet.replies.length}</span>
         </div>
-        <div>
-          Likes: <span>tweet.likes</span>
+        <div className="my-2 rounded-lg bg-slate-600 p-2">
+          Like: 0 (Coming Soon)
         </div>
       </div>
     </div>
