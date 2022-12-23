8357ee5c53b1e229de891213f22108a264474399
victorsmith <victor7smith@gmail.com>
2022-12-23 01:44:35 -0700

    post tweet works now

--- components/newPost.js
+++ components/newPost.js
@@ -1,44 +1,57 @@
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
+import React, { useState } from "react";
+import constants from "../constants/constants";
 
 export default function NewPost() {
-  ``;
+  const router = useRouter();
 
-  const [tweets, setTweets] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    // Prevents form from refreshing the page
-    e.preventDefault();
+  const api = constants.apiBaseUrl;
+  console.log("ApiBaseUrl:", api);
 
+  const handleSubmit = async (event) => {
+    setLoading(true);
+
+    event.preventDefault();
+
+    // Get data from the form.
     const data = {
-      tweetBody: e.target.tweet.value,
+      content: event.target.tweet.value,
     };
 
-    setTweets((tweetHistory) => [...tweetHistory, data]);
+    console.log("data", data);
+
+    const jwt =
+      "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTRlZmFiMjAwODJjMzFjNTc2OTNiNiIsImlhdCI6MTY3MTc1MzY0MzkxMywiZXhwIjoxNjcxNzUzNzMwMzEzfQ.pZDaGYP4MvY47rh8hBYamu01wak7n7q0g8X7uCusVU7JR1kZ2ikxHGXNUn-E_Ed3AeojI2zJPjIttrMhiR8rz3t4-Jq3imVEbkwgIoraxh5TvSqAP2QfpaHQMulMyLFw6pwI1KrWNgXWsRacHfOiYRqQ3l8lgPy2AkrNbn8MyuLmgCpcxvcNMDOGnSfL1L4gFpwnpwv9ezxUMdb49REblIrZkx1BLx7IOC-IR7372v78uFT2g69KyvPGMC68g0eFT8A64FbUtK8ZNrACliOhBaCFw9Egxc-4eID-go3NMatUibFm5AwEDzIx2kFb2taK8Ri2BWnMvDkNQE_ZD7TPzwPeMhdLkE4uF1ZOqddUtetPnUxdPPzMkgASssqCAyKjB3HHdN1cq4wv6jjgDFFLPLzQDQm2Va_Plr80r_hyL8AGSnr9K9bGkS0-vplFgkrxxbtiuoWqdsCwUx0suF_5YH1HeBoqD2UaUcR-rU0ea0N-060TYtADQwq-baHpbP4QiJghZZZ0cn5irxp8p4v_XDZmBO1zuB6st9chgrvi-Rvvt1dIQVLtL-XRwsRMEIxzzdz0QMQnkUsMu0P0Ivheg3a7lvuhDplyWLOyxvxtadYdLGnvwI7UC4n2sp0d899BEi4XQM_DLnCXrXhxN9WuMwY3oAV2_bpOwMPodRw_-xk";
+    const api = constants.apiBaseUrl;
+    console.log("ApiBaseUrl", api);
+
+    const endpoint = `${api}/tweets/`;
+    const options = {
+      method: "POST",
+      // Tell the server we're sending JSON.
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: jwt,
+      },
+      body: JSON.stringify({
+        content: data.content,
+      }),
+    };
+
+    // Fetch all tweets
+    const res = await fetch(endpoint, options);
+    console.log("Res (post tweet)", res)
   };
 
-  useEffect(() => {
-    console.log("Form result:", tweets);
-  }, [tweets]);
-
   return (
-    <div className="m-4 flex flex-col rounded-md bg-slate-800 p-4">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="tweet"></label>
-        <input
-          type="text"
-          // value="What's on your mind?"
-          id="tweet"
-          name="tweet"
-          required
-          minLength="1"
-          maxLength="280"
-        ></input>
-
+    <div className="flex flex-col">
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <label htmlFor="tweet">Tweet</label>
+        <input type="textarea" id="tweet" name="tweet" required />
         <button type="submit">Tweet</button>
-
-        {/* RegEx: pattern="[a-z0-9]{1,15}" */}
       </form>
     </div>
   );
